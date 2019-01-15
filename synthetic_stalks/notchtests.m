0d75fd1127d4d549c382a486d5665fd5dd787f58
ryantheengineer <ryan.larson.wa@gmail.com>
2019-01-15 12:07:37 -0700

    Got a semi-working notch, with sharp corners

--- synthetic_stalks/notchtests.m
+++ synthetic_stalks/notchtests.m
@@ -9,12 +9,21 @@ if nwidth >= dmaj
     error('Notch width is too large. Increase dmaj or decrease nwidth.')
 end
 
-theta = linspace(-pi,pi,100);
+theta = linspace(-pi,pi,100);   % The range of theta must stay as -pi to pi
 yshift = zeros(1,length(theta));
+notch = zeros(1,length(theta));
 y = (dmin/2)*cos(theta);
 x = (dmaj/2)*sin(theta);
 % flatlimit = pi/4;
 flatlimit = asin(nwidth/dmaj);
+for i = 1:length(notch)
+    if theta(i) <= -flatlimit | theta(i) >= flatlimit
+        notch(i) = NaN;
+    else
+        notch(i) = -(0.1*(cos((2*pi/nwidth)*theta(i))) + 0.1);
+    end
+end
+
 % flatheight = 0;
 
 for i = 1:length(yshift)
@@ -22,7 +31,8 @@ for i = 1:length(yshift)
     if theta(i) < -pi | theta(i) > pi
         yshift(i) = 0;
     elseif theta(i) >= -flatlimit & theta(i) <= flatlimit
-        yshift(i) = (dmin/2)*cos(-flatlimit);
+%         yshift(i) = (dmin/2)*cos(-flatlimit);   % Keep the same value as the value at flatlimits
+        yshift(i) = y(i) + notch(i);
     else
         yshift(i) = y(i);
     end          
@@ -37,11 +47,16 @@ end
 % x(1) = 0;
 % x(end) = 0;
 
-figure(1)
-plot(theta,y)
-hold on
-plot(theta,yshift)
+% figure(1)
+% plot(theta,y)
+% hold on
+% plot(theta,yshift)
+% legend('y','yshift');
+% xlabel('\theta, radians')
 
 hold off
 figure(2)
 plot(x,yshift)
+
+figure(3)
+plot(theta,notch)
