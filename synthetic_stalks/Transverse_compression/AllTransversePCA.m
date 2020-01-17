6ffc64f9a3ca35696e33eb97264921483df51ad4
ryantheengineer <ryan.larson.wa@gmail.com>
2020-01-17 09:00:13 -0700

    Added AllTransversePCA.m to get a single .mat file with all PCA and stalk indexing data for transverse_wrapper_V4.m

---
+++ synthetic_stalks/Transverse_compression/AllTransversePCA.m
@@ -0,0 +1,31 @@
+function AllTransversePCA()
+% FILENAME: AllTransversePCA.m
+% AUTHOR: Ryan Larson
+% DATE: 1/17/2020
+%
+% PURPOSE: 
+% 
+% 
+% INPUTS:
+%       
+%       
+% OUTPUTS:
+%       
+%
+%
+% NOTES: 
+%       
+% 
+% 
+% VERSION HISTORY:
+% V1 - 
+% V2 - 
+% V3 - 
+%
+% -------------------------------------------------------------------------
+
+
+
+
+
+end
